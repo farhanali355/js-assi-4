e8c84a0a0bda660ae698570c4203527d29788488
farhan <ff1917914@gmail.com>
2024-12-04 04:44:13 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// let arr = [
+//     42,
+//     true,
+//     "hello world",
+//     {
+//         name: "jhon",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "javascript is fun!"
+// ];
+
+// console.log(arr);
+
+
+// Q1 ////////////////////
+
+// let stringArr = arr.map(value => console.log(value.toString()));
+
+// Q2/////////////////////
+
+
+// let num = arr.filter((a)=>{
+
+//    return a<50
+
+// });
+
+// console.log(num);
+
+
+// Q3/////////////////////
+
+
+// let each = arr.forEach( value => console.log(value[3]));
+
+
+// Q4/////////////////////
+
+
+// let red = arr.reduce(value => console.log(value));
+
+
+// Q5/////////////////////
+
+
+// let fin = arr.find((value) => {
+//     let boolen = true;
+//     value = boolen;
+// console.log(value);
+
+// })
+
+
+
+
+// Q6///////////////////////////
+
+// let  index = arr.findIndex(element => typeof element === 'object' && element !== null);
+// console.log(index);
+
+// Q7//////////////////////
+
+
+// let somme = arr.some(num=>{
+//     return num >= 42;
+
+// });
+// console.log(somme);
+
+
+// Q8/////////////////////
+
+// let allStrings = arr.every(element => typeof element === 'string');
+
+// console.log(allStrings); 
+
+
